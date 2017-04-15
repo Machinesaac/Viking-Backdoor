f91f5713b5456d4484915d7a2693db7c46bcaffa
Black Viking <yakuza3003@yandex.com>
2017-04-15 15:35:25 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#
+
+__author__ = "Black Viking"
+__date__   = "15.04.2017"
+
+import os, sys, pip, subprocess
+
+dirs = os.listdir(os.getcwd())
+
+def installModule(module):
+	pip.main(['install', module])
+	print "=" * 60
+
+def main():
+	try:
+		import mss
+	except ImportError:
+		 if raw_input("[!] Mss module not found. Do you want install it?[Y/n] --> ").lower() == "y":
+		 	installModule("mss")
+		 else:
+		 	sys.exit()
+
+	try:
+		import colorama
+	except ImportError:
+		 if raw_input("[!] Colorama module not found. Do you want install it?[Y/n] --> ").lower() == "y":
+		 	installModule("colorama")
+		 else:
+		 	sys.exit()
+
+	if "pyinstaller.exe" not in os.listdir(sys.exec_prefix + os.sep + "Scripts"):
+		 if raw_input("[!] PyInstaller module not found. Do you want install it?[Y/n] --> ").lower() == "y":
+		 	installModule("pyinstaller")
+		 else:
+		 	sys.exit()
+
+	else:
+		pass
+
+	if "downloads" not in dirs:
+		os.mkdir("downloads")
+		print "[~] Directory created ==> 'downloads'"
+	else:
+		pass
+
+	if "screenshots" not in dirs:
+		os.mkdir("screenshots")
+		print "[~] Directory created ==> 'screenshots'"
+
+	else:
+		pass
+
+	if "executables" not in dirs:
+		os.mkdir("executables")
+		print "[~] Directory created ==> 'executables'"
+
+	subprocess.Popen([sys.executable, 'PyRAT.py'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+	sys.exit()
+
+if __name__ == "__main__":
+	main()
